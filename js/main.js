74a53d357aff4d9bd2bac387436d59dd92e2a2ac
Alexandru Nastase <axn778@cs.bham.ac.uk>
2020-07-14 18:34:07 +0100

    Responsive landing

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 menu = document.getElementsByClassName("-menu")[0];
+hamburger = menu.getElementsByTagName("i")[0];
 menu.onclick = () => {
   document.getElementsByClassName("-div-nav")[0].classList.toggle("-active");
+  hamburger.classList.toggle("fa-times");
 };
